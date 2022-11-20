28f6dc792a7228855c80ae1a0f08c10dfacd4b1d
matyukha <m@matyuha.ru>
2022-11-20 22:50:40 +0300

    added second test

--- test/test_search_wikipedia.py
+++ test/test_search_wikipedia.py
@@ -5,14 +5,20 @@ from selene.support.shared import browser
 
 
 def test_search():
-
-    with step('Search for content'):
+    with step('Search and verify for Wikipedia'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, 'Search Wikipedia')).click()
         browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/search_src_text')).type(
             'QA'
         )
+        browser.all(
+            (AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title')
+        ).should(have.size_greater_than(0))
 
-    with step('Content should be found'):
+    with step('Search and verify for BrowserStack'):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, 'Search Wikipedia')).click()
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/search_src_text')).type(
+            'BrowserStack'
+        )
         browser.all(
             (AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title')
         ).should(have.size_greater_than(0))
