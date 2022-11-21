f03fa8ae9acbc0b9ddfe735cb6640215083607b8
matyukha <m@matyuha.ru>
2022-11-21 23:11:05 +0300

    added one more test

--- test/test_search_wikipedia.py
+++ test/test_search_wikipedia.py
@@ -4,6 +4,13 @@ from selene import have
 from selene.support.shared import browser
 
 
+def test_open_register_form():
+    with step('Open register form'):
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/menu_overflow_button')).click()
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/explore_overflow_account_name')).click()
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/login_create_account_link')).click()
+
+
 def test_search():
     with step('Search and verify for Microsoft'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, 'Search Wikipedia')).click()
@@ -15,7 +22,7 @@ def test_search():
         ).should(have.size_greater_than(0))
 
     with step('Search and verify for BrowserStack'):
-        browser.element((AppiumBy.ACCESSIBILITY_ID, 'Search Wikipedia')).click()
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/search_src_text')).clear()
         browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/search_src_text')).type(
             'BrowserStack'
         )
@@ -23,9 +30,3 @@ def test_search():
             (AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title')
         ).should(have.size_greater_than(0))
 
-
-def test_open_register_form():
-    with step('Open register form'):
-        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/menu_overflow_button')).click()
-        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/explore_overflow_account_name')).click()
-        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/login_create_account_link')).click()
